a26e0e711726ef278a7fb849f18a1b0959086647
Frank <f.dersjant@gmail.com>
2022-11-05 16:43:49 +0100

    app

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"example/rest/Domain"
 	"fmt"
+
+	app "example/rest/App"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	customer := Domain.Customer{ID: "1", Name: "John", Address: "123 Main St", DateofBirth: "01/01/2000", Status: "Active"}
+	fmt.Println("Starting App")
 
-	fmt.Println(customer)
+	app.Start()
+
+	//	App.app.Start()
+
 }
